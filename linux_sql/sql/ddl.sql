cc0f14f0803d9bd07abca4cd7136ebadd64ce7cc
Miguel Dario <miguelagustin.dario@gmail.com>
2022-07-22 18:14:51 +0000

    Update ddl.sql

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -17,6 +17,6 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_usage (
     cpu_idle INT NOT NULL,
     cpu_kernel INT NOT NULL,
     disk_io INT NOT NULL,
-    disk_available INT NOT NULL;
-    FOREIGN KEY (host_id) REFERENCES host_info (id),
+    disk_available INT NOT NULL,
+    FOREIGN KEY (host_id) REFERENCES host_info (id)
 );
