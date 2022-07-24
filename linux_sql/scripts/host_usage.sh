d3442bcbdde8c6cde22252198bb0748fdf6a5f3d
Miguel Dario <miguelagustin.dario@gmail.com>
2022-07-24 21:12:13 +0000

    Add missing comma to insert statement

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -25,7 +25,7 @@ timestamp=$(vmstat -t | awk '{print $18 " " $19}' | tail -1 | xargs)
 
 host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
 
-insert_stmt="INSERT INTO host_usage(timestamp host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
+insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
 VALUES($timestamp, $host_id, $memory_free, $memory_free, $cpu_idle, $cpu_kernel, $disk_io, $disk_available)"
 
 export PGPASSWORD=$psql_password
