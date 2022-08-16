2b91341af9ccb0f9626d62c80e28192ab02d2761
Miguel Dario <miguelagustin.dario@gmail.com>
2022-08-16 20:50:04 +0000

    Finish all questions

--- python_data_analytics/psql/data_explore.sql
+++ python_data_analytics/psql/data_explore.sql
@@ -1,3 +1,6 @@
+-- Show Database Schema
+\d+ retail;
+
 -- Q1: Show first 10 rows
 SELECT *
 FROM retail
@@ -12,7 +15,9 @@ SELECT COUNT(DISTINCT customer_id)
 FROM retail;
 
 -- Q4: invoice date range (e.g. max/min dates)
-SELECT MAX(invoice_date) AS max, MIN(invoice_date) AS min
+SELECT
+    MAX(invoice_date) AS max,
+    MIN(invoice_date) AS min
 FROM retail;
 
 -- Q5: number of SKU/merchants (e.g. unique stock code)
@@ -20,10 +25,24 @@ SELECT COUNT(DISTINCT stock_code)
 FROM retail;
 
 -- Q6: Calculate average invoice amount excluding invoices with a negative amount (e.g. canceled orders have negative amount)
-SELECT COUNT(DISTINCT invoice_no)
+SELECT AVG(total_amount)
+FROM(
+    SELECT SUM(unit_price * quantity) as total_amount
+    FROM retail
+    GROUP BY invoice_no
+    HAVING SUM(unit_price * quantity) > 0
+    ) AS sub;
+
+-- Q7: Calculate total revenue (e.g. sum of unit_price * quantity)
+SELECT SUM(unit_price * quantity)
 FROM retail;
 
-SELECT invoice_no, SUM(unit_price)
+-- Q8: Calculate total revenue by YYYYMM
+SELECT
+    CAST(EXTRACT(YEAR FROM invoice_date) AS INTEGER) * 100 + CAST(EXTRACT(MONTH FROM invoice_date) AS INTEGER) AS yyyymm,
+    SUM(unit_price * quantity)
 FROM retail
-GROUP BY invoice_no;
+GROUP BY yyyymm
+ORDER BY yyyymm;
+
 
